a931c1c0bdd2660348fdd82adf658f0a78059d44
Lukas Grotz <lukasgrotz@gmail.com>
2017-12-22 20:24:55 +0100

    Transmitting Light-Value over Serial

--- DFRobot/src/main.cpp
+++ DFRobot/src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
-
 #include <DFRobot2WD.h>
 #include <Notes.h>
 
 DFRobot2WD robot = DFRobot2WD();
 
 void setup() {
-
+    Serial.begin(9600);
+    Serial.println("Interfacing with NodeMCU");
 }
 
 void loop() {
-
+    Serial.println("Sending Data");
+    Serial.print("{\"light\":");
+    Serial.print(robot.getLight());
+    Serial.print("}");
+    Serial.println();
+    delay(200);
 }
