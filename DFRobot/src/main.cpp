6edb92809ebfed421f4e5d6e579c2b991716330f
Lukas Grotz <lukasgrotz@gmail.com>
2017-12-23 19:26:07 +0100

    NodeMCU controls DFRobot via Serial commands
    Also, the nodemcu gets its commands via websocket, so thats cool.

--- DFRobot/src/main.cpp
+++ DFRobot/src/main.cpp
@@ -3,6 +3,7 @@
 #include <Notes.h>
 
 DFRobot2WD robot = DFRobot2WD();
+int code = 0;
 
 void setup() {
     // set up Serial connection
@@ -10,6 +11,37 @@ void setup() {
 }
 
 void loop() {
-    Serial.println(robot.getLight());
-    delay(1000);
+    // Clears inData String every loop
+    String inData = "";
+
+    // New Serial data available
+    if (Serial.available())
+    {
+        inData = Serial.readStringUntil('\r'); // Read line
+        Serial.flush();
+        if (inData == "forward")
+        {
+            robot.motorControl(FORWARD, 100, FORWARD, 100);
+            delay(250);
+            robot.motorStop();
+        }
+        if (inData == "backward")
+        {
+            robot.motorControl(BACKWARD, 100, BACKWARD, 100);
+            delay(250);
+            robot.motorStop();
+        }
+        if (inData == "left")
+        {
+            robot.motorControl(FORWARD, 100, BACKWARD, 100);
+            delay(250);
+            robot.motorStop();
+        }
+        if (inData == "right")
+        {
+            robot.motorControl(BACKWARD, 100, FORWARD, 100);
+            delay(250);
+            robot.motorStop();
+        }
+    }
 }
--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -3,46 +3,101 @@
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
 
-//WiFi SSID and Password
+// WiFi SSID and Password
 const char *ssid = "ssid";
-const char *password = "pass";
+const char *password = "password";
 
-WiFiClient client;
 HTTPClient http;
-SoftwareSerial swSer(14, 12, false, 256);
+WiFiServer server(80);
+
+SoftwareSerial swSer(D0, D1, false, 256);
 
 const char *host = "yourServerIP";
+
+int PostData(String inData) {
+    http.begin(host);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    return http.POST("data=" + inData); // Send the request
+    http.end(); // close connection
+}
 
 void setup() {
     // Set up Serial and Wifi connection
     Serial.begin(9600);
     swSer.begin(9600);
+    // Connect to WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
     WiFi.begin(ssid, password);
-    // Let module initialize
-    delay(1000);
-    int retries = 0;
-    // Attempt to connect to WiFi 14 times before timing out
-    while ((WiFi.status() != WL_CONNECTED) && (retries < 15))
+
+    while (WiFi.status() != WL_CONNECTED)
     {
-        retries++;
         delay(500);
+        Serial.print(".");
     }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    // Start the server
+    server.begin();
+    Serial.println("Server started");
+
+    // Print the IP address
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-    // Clear Serial data every loop
-    String inData = "";
-    if (swSer.available()) // Check if new Serial data is available
-    {
-        inData = swSer.readStringUntil('\n'); // Reads data and terminates string at newline
-        Serial.println(inData);
-        http.begin(host);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        int httpCode = http.POST("data="+inData); // Send the request
-        String payload = http.getString();     // Get the response payload
-        Serial.println(httpCode); // Print HTTP return code
-        Serial.println(payload);  // Print request response payload
-        http.end(); // close connection
+    // Check if a client has connected
+    WiFiClient client = server.available();
+    if(client) {
+        delay(100);
+        if (client.available()) {
+            // Read the first line of the request
+            String req = client.readStringUntil('\r');
+            client.flush();
+            // Match the request
+            if (req.indexOf("") != -10) { //checks if you're on the main page
+                if (req.indexOf("/forward") != -1) { 
+                    Serial.println("You clicked forward");
+                    swSer.println("forward");
+                }
+                if (req.indexOf("/backward") != -1) { 
+                    Serial.println("You clicked backward");
+                    swSer.println("backward");
+                }
+                if (req.indexOf("/left") != -1)
+                { 
+                    Serial.println("You clicked left");
+                    swSer.println("left");
+                }
+                if (req.indexOf("/right") != -1)
+                { 
+                    Serial.println("You clicked right");
+                    swSer.println("right");
+                }
+            }
+            else {
+                Serial.println("invalid request");
+                client.stop();
+            }
+            // Prepare the response String
+            String s = "HTTP/1.1 200 OK\r\n";
+            s += "Content-Type: text/html\r\n\r\n";
+            s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+            s += "<br><input type=\"button\" name=\"bl\" value=\"Forward\" onclick=\"location.href='/forward'\">"; // Forward-button
+            s += "<br>";
+            s += "<br><input type=\"button\" name=\"bl\" value=\"Left\" onclick=\"location.href='/left'\">"; // Left-button
+            s += "<br><input type=\"button\" name=\"bl\" value=\"Right\" onclick=\"location.href='/right'\">"; // Right-button
+            s += "</html>\n";
+            s += "<br>";
+            s += "<br><input type=\"button\" name=\"bl\" value=\"Backward\" onclick=\"location.href='/backward'\">"; // Backward-button
+            client.flush();
+            // Send the response to the client
+            client.print(s);
+        }
     }
 }
